d7d1e87165ed03a391dfd97925a04fed51027611
Mohammed Zaheer <152991044+MohammedZaheer0@users.noreply.github.com>
2024-10-07 13:57:29 +0530

    commited

--- src/components/Services.js
+++ src/components/Services.js
@@ -145,14 +145,14 @@ const Services = () => {
 
         <h2 className='font-bold text-[#898A91] tracking-widest'>SERVICES</h2>
         <h1 className='lg:my-10 my-7 font-bold lg:text-6xl text-3xl md:text-4xl md:w-[70%] leading-tight'>Building great experiences involves a lot of moving parts.</h1>
-        <h3 className='font-bold md:text-3xl text-2xl lg:mt-28 mt-16 md:mb-10 mb-5'>We bring it all together.</h3>
-        <p className='lg:w-[55%] md:w-[65%] text-[#898A91] text-[20px] font-bold'>We're just as happy working on a special project that needs to be done right as we are building an end-to-end digital experience. Below are just some of the things we do.</p>
+        <h3 className='font-bold md:text-3xl text-2xl lg:mt-28 mt-16 md:mb-10 mb-5'>Welcome to [Your Company Name]</h3>
+        <p className='lg:w-[55%] md:w-[65%] text-[#898A91] text-[20px] font-bold'>Your One-Stop Solution for All Your Digital and Offline Marketing Needs</p>
       </div>
 
 
 
 
-      <div className='lg:my-28'>
+      {/* <div className='lg:my-28'>
           
           <div className='grid md:grid-cols-2 grid-cols-1 justify-between items-start lg:pt-36 pt-10 md:pb-10 lg:pb-20'>
               <div>
@@ -216,7 +216,175 @@ const Services = () => {
           </div>
 
 
+      </div> */}
+
+
+
+      <div className='lg:my-28'>
+          
+          <div className='lg:pt-36 pt-10 md:pb-10 lg:pb-20'>
+
+              <div className='my-16'>
+                  <h1 className='font-bold md:text-3xl text-2xl my-5'>Website Development</h1>
+                  <h3 className='md:w-[90%] lg:w-[80%] text-[20px] font-medium my-5'>Whether you need a simple website or a robust e-commerce platform, we offer tailored website development solutions to suit your business goals. From user-friendly designs to seamless functionality, we create responsive, SEO-friendly websites to boost your online presence.</h3>
+
+                  <ol className='list-disc pl-4'>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Custom Web Design
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      E-commerce Solutions
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      CMS Development (WordPress, Shopify, etc.)
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Website Maintenance and Support
+                    </li>
+                  </ol>
+
+              </div>
+
+
+              <div className='my-16'>
+                <h1 className='font-bold md:text-3xl text-2xl my-5'>Hosting Services</h1>
+                <h3 className='md:w-[90%] lg:w-[80%] text-[20px] font-medium my-5'>Reliable and secure hosting is crucial for your website's performance. We offer scalable hosting solutions with 99.9% uptime and complete technical support to ensure your website runs smoothly.</h3>
+                <ol className='list-disc pl-4'>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Shared, VPS & Dedicated Hosting
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Domain Registration
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      24/7 Support & Monitoring
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Data Backups & Security
+                    </li>
+                  </ol>
+
+              </div>
+
+
+              <div className='my-16'>
+                <h1 className='font-bold md:text-3xl text-2xl my-5'>Digital Marketing</h1>
+                <h3 className='md:w-[90%] lg:w-[80%] text-[20px] font-medium my-5'>Grow your business with our comprehensive digital marketing services. From Social Media Ads to Google Ads, we develop customized campaigns to increase your reach and drive conversions.</h3>
+                <ol className='list-disc pl-4'>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Social Media Advertising: Facebook, Instagram, LinkedIn, and more.
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Google Ads: Maximize visibility on Google search and display networks.
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Email Marketing: Build customer relationships with targeted campaigns.
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Content Creation: Compelling visuals and copy to attract your audience.
+                    </li>
+                  </ol>
+
+              </div>
+
+
+              <div className='my-16'>
+                <h1 className='font-bold md:text-3xl text-2xl my-5'>Search Engine Optimization (SEO)</h1>
+                <h3 className='md:w-[90%] lg:w-[80%] text-[20px] font-medium my-5'>Get found by your customers with our strategic SEO services. We optimize your website’s performance, content, and structure to improve your search engine rankings and drive organic traffic.</h3>
+                <ol className='list-disc pl-4'>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Keyword Research & Analysis
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      On-Page & Off-Page Optimization
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Link Building & Content Strategy
+                    </li>
+                    <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                      Technical SEO Audits
+                    </li>
+                  </ol>
+
+              </div>
+
+
+              <div className='my-16'>
+                <h1 className='font-bold md:text-3xl text-2xl my-5'>Flexi Printing Services</h1>
+                <h3 className='md:w-[90%] lg:w-[80%] text-[20px] font-medium my-5'>Need high-quality prints for banners, posters, or outdoor marketing? Our Flexi Printing services provide vibrant, durable, and weather-resistant materials to make your brand stand out.</h3>
+                <ol className='list-disc pl-4'>
+                  <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                    Custom Flex Banners
+                  </li>
+                  <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                    Posters, Billboards, and Standees
+                  </li>
+                  <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                    Signage for Events & Promotions
+                  </li>
+                </ol>
+
+              </div>
+
+
+              <div className='my-16'>
+                <h1 className='font-bold md:text-3xl text-2xl my-5'>Offline Marketing</h1>
+                <h3 className='md:w-[90%] lg:w-[80%] text-[20px] font-medium my-5'>Leverage the power of offline marketing with our creative and strategic solutions. We offer everything from flyer distribution to direct mail campaigns, ensuring your message reaches the right audience.</h3>
+                <ol className='list-disc pl-4'>
+                  <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                    Print Media (Brochures, Flyers, Business Cards)
+                  </li>
+                  <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                    Event Marketing & Promotional Materials
+                  </li>
+                  <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                    Billboards, Signage, and Out-of-home Advertising
+                  </li>
+                </ol>
+
+              </div>
+
+          </div>
+
       </div>
+
+
+
+
+          
+          <div className='lg:pb-14 md:pb-10 pb-8'>
+
+              <div className='my-16'>
+                  <h1 className='font-bold md:text-3xl text-2xl my-5'>Why Choose Us?</h1>
+
+                  <ol className='list-disc pl-4'>
+                      <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                        Experienced Team: A skilled team of developers, designers, and marketing experts.
+                      </li>
+                      <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                        Customized Solutions: Tailored services to fit your business goals.
+                      </li>
+                      <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                        Affordable Pricing: Competitive pricing without compromising on quality.
+                      </li>
+                      <li className='font-bold text-[20px] my-3 text-[#D35400] hover:text-[#fc9652] cursor-pointer w-fit'>
+                        24/7 Support: Dedicated support team to assist you whenever needed.
+                      </li>
+                </ol>
+
+                  
+                  
+                  
+                 
+              </div>
+
+              <div className='my-16'>
+                  <h1 className='font-bold md:text-3xl text-2xl my-5'>Get in Touch Today!</h1>
+                  <h3 className='md:w-[90%] lg:w-[80%] text-[20px] font-medium my-5'>Let us help you take your business to the next level with our all-in-one digital and offline marketing solutions. Contact us for a free consultation and discover how we can grow your brand.</h3>
+
+              </div>
+      </div>
+
 
     </div>
   );
